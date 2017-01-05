1d7e35b7266db21f0e60be17ce2dd9aa661167ae
GaiYu0 <yg1246@nyu.edu>
2017-01-05 14:28:51 +0800

    cv/hyper-network/mnist.py

--- cv/hyper-network/mnist.py
+++ cv/hyper-network/mnist.py
@@ -1,36 +1,74 @@
-from mxnet.initializer import Xavier
+import cPickle as pickle
+import sys
 
+import mxnet as mx
+from mxnet.initializer import Xavier
+from lr_scheduler import FactorScheduler
 from facility import load_mnist
 from mx_layers import *
 from mx_solver import MXSolver
 
-data = load_mnist(shape=(1, 28, 28))
+try: mode = sys.argv[1]
+except: mode = 'hyper'
 
 network = variable('data')
 network = convolution(network, (7, 7), 16, (1, 1), (3, 3))
 network = ReLU(network)
 network = pooling(network, 'maximum', (2, 2), (2, 2))
-network = convolution(network, (7, 7), 16, (1, 1), (3, 3))
+
+if mode == 'normal':
+  weight = variable('convolution1_weight', shape=())
+else:
+  # weight shape (FILTER_OUT, FILTER_IN, WIDTH, HEIGHT)
+  N_z = 4
+  d = N_z
+  FILTER_IN = 16
+  FILTER_OUT = 16
+  WIDTH = 7
+  HEIGHT = 7
+  weight = variable('embedding', shape=(1, N_z))
+  weight = fully_connected(weight, FILTER_IN * d)
+  weight = reshape(weight, (FILTER_IN, d))
+  weight = fully_connected(weight, FILTER_OUT * WIDTH * HEIGHT)
+  weight = reshape(weight, (FILTER_IN, FILTER_OUT, WIDTH, HEIGHT))
+  weight = swap_axes(weight, 0, 1)
+
+network = convolution(network, (7, 7), 16, (1, 1), (3, 3), weight=weight)
+# network = convolution(network, (7, 7), 16, (1, 1), (3, 3))
 network = ReLU(network)
 network = pooling(network, 'maximum', (2, 2), (2, 2))
 network = flatten(network)
 network = fully_connected(network, 10)
 network = softmax_loss(network)
 
+class Initializer(Xavier):
+  def _init_default(self, arg, array):
+    if arg == 'embedding':
+      mx.random.normal(0, 0.01, out=array)
+
+data = load_mnist(shape=(1, 28, 28))
+
+BATCH_SIZE = 1000
+lr = 0.005
+
 optimizer_settings = {
-  'initial_lr' : 0.05,
-  'optimizer'  : 'SGD',
+  'initial_lr' : lr,
+  'optimizer'  : 'Adam',
+  'lr_scheduler' : FactorScheduler(lr, 0.99, data[0].shape[0] // BATCH_SIZE),
 }
 
 solver = MXSolver(
-  batch_size = 1000,
-  data = data,
+  batch_size = BATCH_SIZE,
   devices = (0,),
-  epochs = 5,
-  symbol = network,
-  initializer = Xavier(),
+  epochs = 50,
+  initializer = Initializer(),
   optimizer_settings = optimizer_settings,
+  symbol = network,
+  verbose = False,
 )
 
-test_accuracy, progress = solver.train()
-print test_accuracy, progress
+identifier = 'mnist-%s-network' % mode
+info = solver.train(data)
+pickle.dump(info, open('info/%s' % identifier, 'wb'))
+parameters = solver.export_parameters()
+pickle.dump(parameters, open('parameters/%s' % identifier, 'wb'))
