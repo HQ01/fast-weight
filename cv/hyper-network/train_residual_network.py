131696fcdd2109cc662f98c7b31d1c5f21075abd
Gai Yu <yg1246@nyu.edu>
2017-01-10 00:40:42 +0800

    train_residual_network.py

--- cv/hyper-network/train_residual_network.py
+++ cv/hyper-network/train_residual_network.py
@@ -10,22 +10,30 @@ from mx_solver import MXSolver
 
 from residual_network import triple_state_residual_network
 
-BATCH_SIZE = 128
 # MODES = {'mode' : 'normal'}
 MODES = {'mode' : 'weight-sharing'}
 # MODES = {'mode' : 'hyper', 'embedding' : 'feature_map', 'batch_size' : BATCH_SIZE}
 N = int(sys.argv[1])
 network = triple_state_residual_network(N, **MODES)
 
-lr = 0.1
-lr_table = {32000 : lr * 0.1, 48000 : lr * 0.01}
-
-data = load_cifar10_record(BATCH_SIZE)
+if MODES['mode'] is 'normal':
+  BATCH_SIZE = 128
+  lr = 0.1
+  lr_table = {32000 : lr * 0.1, 48000 : lr * 0.01}
+  lr_scheduler = AtIterationScheduler(lr, lr_table)
+  epochs = 150
+elif MODES['mode'] is 'weight-sharing':
+  BATCH_SIZE = 64
+  lr = 0.01
+  lr_table = {40 : 0.001, 50 : 0.0001}
+  lr_scheduler = AtEpochScheduler(lr, lr_table, 50000, BATCH_SIZE)
+  epochs = 60
+elif MODES['mode'] is 'hyper': pass # TODO
 
 optimizer_settings = {
   'args'         : {'momentum' : 0.9},
   'initial_lr'   : lr,
-  'lr_scheduler' : AtIterationScheduler(lr, lr_table),
+  'lr_scheduler' : lr_scheduler,
   'optimizer'    : 'SGD',
   'weight_decay' : 0.0001,
 }
@@ -33,20 +41,20 @@ optimizer_settings = {
 solver = MXSolver(
   batch_size = BATCH_SIZE,
   devices = (0, 1, 2, 3),
-  epochs = 1,
+  epochs = epochs,
   initializer = PReLUInitializer(),
   optimizer_settings = optimizer_settings,
   symbol = network,
   verbose = True,
 )
 
+data = load_cifar10_record(BATCH_SIZE)
 info = solver.train(data)
 
-# TODO
-identifier = 'triple-state-%s-residual-network-%d' % (MODES['mode'], N) 
-# identifier = 'triple-state-%s-residual-network-%d-%s-embedding' % (MODES['mode'], N, MODES['embedding'])
+if MODES['mode'] is 'normal' or MODES['mode'] is 'normal' is 'weight-sharing':
+  identifier = 'triple-state-%s-residual-network-%d' % (MODES['mode'], N)
+elif MODES['mode'] is 'hyper':
+  identifier = 'triple-state-%s-residual-network-%d-%s-embedding' % (MODES['mode'], N, MODES['embedding'])
 pickle.dump(info, open('info/%s' % identifier, 'wb'))
 parameters = solver.export_parameters()
-for p in sorted(parameters[0].keys()):
-  print p
 pickle.dump(parameters, open('parameters/%s' % identifier, 'wb'))
