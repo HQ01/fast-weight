19306914973b640919b30e52875840f95a8c5903
GaiYu0 <yg1246@nyu.edu>
2017-03-19 15:44:01 +0800

    decaying-attention-network.ipynb

--- cv/hyper-network/train_cifar_decayed_attention_network.sh
+++ cv/hyper-network/train_cifar_decayed_attention_network.sh
@@ -1,4 +1,4 @@
-for decaying_rate in 0 0.5 1 1.5 2 2.5
+for decaying_rate in 3.0
 do
   echo $decaying_rate
   ipython train_cifar_decayed_attention_network.py -- --n_layers=9 --decaying_rate=$decaying_rate --gpu=0,1,2,3
