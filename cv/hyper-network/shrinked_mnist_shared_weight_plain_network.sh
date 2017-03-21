ce3c58641ab4dc831ef67e106823288cb4e85ee0
Gai Yu <yg1246@nyu.edu>
2017-03-22 06:26:39 +0800

    weight-sharing

--- cv/hyper-network/shrinked_mnist_shared_weight_plain_network.sh
+++ cv/hyper-network/shrinked_mnist_shared_weight_plain_network.sh
@@ -4,7 +4,7 @@ do
   do
     postfix=round-$((gpu_index + $1 * 4))
     echo $postfix
-    ipython shrinked_mnist_plain_network.py -- \
+    ipython shrinked_mnist_shared_weight_plain_network.py -- \
       --gpu_index=$gpu_index --n_plain_layers=$n_plain_layers --postfix=$postfix &
   done
   wait
